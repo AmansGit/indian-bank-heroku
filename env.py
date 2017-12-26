112f666df16ed3b888b86bb45a4ca56c23a5be58
Akash <akashkr.sharma36@gmail.com>
2017-12-27 03:45:43 +0530

    minor change

--- env.py
+++ env.py
@@ -1,7 +1,11 @@
-APP_ENV = 'local'
-APP_DEBUG = True
+# for production replace 'local' with 'production'
+APP_ENV = 'local' 
+
+APP_DEBUG = False if APP_ENV == 'production' else True
 
 DATABASE_NAME = 'indian_banks'
 DATABASE_USERNAME = 'postgres'
 DATABASE_PASSWORD = 'password'
 HOST = '127.0.0.1'
+
+Postgres_URL = 'postgres://tredpdzfxmfaaq:21f5c7c984244ba56b89ee5b0f743376f785c4ccffc15701201b01a0ce191f8a@ec2-23-21-220-32.compute-1.amazonaws.com:5432/dpd3nsi4eaong'
